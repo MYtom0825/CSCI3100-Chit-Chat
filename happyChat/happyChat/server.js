c28b15a767131ef8e0ca2daa85d1fd2f9207d604
Matt Yu <www.yukawai@gmail.com>
2021-03-17 01:26:38 +0800

    Update server.js

--- happyChat/happyChat/server.js
+++ happyChat/happyChat/server.js
@@ -16,14 +16,21 @@ var UserAccountSchema=mongoose.Schema({
     username:{type:String,unique:true,require:true},
     password:{type:String,require:true},
     email:{type:String,unique:true,require:true},
-    onOffstatus:{type:String}
+    onOffstatus:{type:String},
+    userProfile:{type:mongoose.Schema.Types.ObjectId,ref:'UserProfile'},
+    misson:{type:mongoose.Schema.Types.ObjectId,ref:'Mission'},
+    report:{type:mongoose.Schema.Types.ObjectId,ref:'Report'},
+    queue:{type:mongoose.Schema.Types.ObjectId,ref:'Queue'}
 });
 
 var UserProfileSchema=mongoose.Schema({
     account:{type:mongoose.Schema.Types.ObjectId,ref:'UserAccount'},
     picture:{type:String},
     nickName:{type:String},
-    gender:{type:String},
+    gender:{type:String,
+            enum:["male","female"],
+            default:"male",
+    },
     university:{type:String},
     faculty:{type:String},
     major:{type:String},
@@ -43,9 +50,32 @@ var MissionSchema=mongoose.Schema({
     missionID:{type:Number},
     topic:{type:String},
     description:{type:String},
-    token:{type:Number}
+    token:{type:Number},
+    userAccount:{type:mongoose.Schema.Types.ObjectId,ref:'UserAccount'}
 });
+
+var ReportSchema=mongoose.Schema({
+    userAccount:{type:mongoose.Schema.Types.ObjectId,ref:'UserAccount'},
+    reporterID:{type:Number},
+    reportedID:{type:Number},
+    reason:{type:String},
+    speakerID:{type:Number},
+    text:{type:String},
+    time:{type:Date}
+})
+
+var QueueSchema=mongoose.Schema({
+    userAccount:{type:mongoose.Schema.Types.ObjectId,ref:'UserAccount'},
+    queueNumber:{type,Number},
+    requiredGender:{type:String},
+    requiredUni:{type:String},
+    requiredMajor:{type:String},
+    requiredYear:{type:Number},
+    requiredStatus:{type:String}
+})
 
 var UserAccount=mongoose.model('UserAccount',UserAccountSchema);
 var UserProfile=mongoose.model('UserProfile',UserProfileSchema);
 var Mission=mongoose.model('Mission',MissionSchema);
+var Report=mongoose.model('Report',ReportSchema);
+var Queue=mongoose.model('Queue',QueueSchema);
