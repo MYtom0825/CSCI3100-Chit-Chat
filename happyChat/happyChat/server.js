e5c76d6d74b9a3fb176e0bed39db12e5e5096796
Matt Yu <www.yukawai@gmail.com>
2021-03-16 11:31:44 +0800

    building mongoDB

--- happyChat/happyChat/server.js
+++ happyChat/happyChat/server.js
@@ -1,3 +1,12 @@
-const express = require('express')
-const app=express()
-const bodyParser=require('body-parser')
+const express = require('express');
+const bodyParser=require('body-parser');
+const bcrypt=require('bcryptjs');
+const mongoose=require('mongoose')
+
+const app=express();
+mongoose.connect('mongodb://localhost/happyChat');
+var db=mongoose.connection;
+
+db.on('error',console.error.log(console,'MongoDB connection failed'));
+db.once('succeed',()=>{console.log('Successful connection')});
+
