a1cda7d6529c4107fb3c6f323d18cde8b872ad73
mattyu122 <46863510+mattyu122@users.noreply.github.com>
2021-03-18 13:23:00 +0800

    update router_login.js
    return json structure:
    loginstate: 0 => can't find user
                      1 => password incorrect
                      2 => login successful

--- happyChat/happyChat/router/router_login.js
+++ happyChat/happyChat/router/router_login.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 let UserAccount = require('../model/model_account.js');
 
@@ -10,8 +11,35 @@ router.post('/login', (req, res) => {
     var username = req.body.username;
     var email = req.body.email;
     var password = req.body.password;
+    
+    UserAccount.findOne({username:username},function(error,user){
+        /*loginstate:0 => can't find user
+                     1 => password incorrect
+                     2 => login successful
+        */ 
+    if(err){
+        var data={
+            'loginstate':0
+        };
+        res.json(data);
+    }
+    if(bcrypt.compareSync(password,user.password)){
+        req.session.loggedin=true;
+        req.session.username=username;
+        //res.redirect('/');
+        var data={
+            'loginstate':2
+        }
+        res.json(data);
+    }else{
+        var data={
+            'loginstate':1
+        }
+        res.json(data);
+    }
+    });
 
-    if (username && email && password) {    // to be modified
+    /*if (username && email && password) {    // to be modified
         //connect to mongodb to check...
         if (true) { //login success
             request.session.loggedin = true;
@@ -26,7 +54,7 @@ router.post('/login', (req, res) => {
     else {
         res.send('Please enter Username, Email and Password!');
         res.end();
-    }
+    }*/
 })
 
 module.exports = router;
