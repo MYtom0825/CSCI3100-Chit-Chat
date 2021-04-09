bb176f0a5481a281e9bb604e6ccf726a0bf87db3
mattyu122 <46863510+mattyu122@users.noreply.github.com>
2021-04-09 23:31:04 +0800

    mongoDB connection

--- happyChat/happyChat/server.js
+++ happyChat/happyChat/server.js
@@ -27,7 +27,7 @@ app.use(main);
 app.use(mission);
 
 //mongodb
-mongoose.connect('mongodb://localhost/happyChat');
+mongoose.connect("mongodb+srv://chit_chat:1230123@cluster0.4syir.mongodb.net/chit_chat?retryWrites=true&w=majority",{useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error',console.error.log(console,'MongoDB connection failed'));
 db.once('succeed',()=>{console.log('Successful connection')});
