f98bd747793899662e064c21d7d25bc0097cb9e5
MYtom0825 <62584411+MYtom0825@users.noreply.github.com>
2021-04-11 01:34:15 +0800

    profileRegisterForm with default value

--- Website/ProfileRegistrationForm/ProfileRegisterForm.js
+++ Website/ProfileRegistrationForm/ProfileRegisterForm.js
@@ -8,12 +8,12 @@ class ProfileRegisterForm extends React.Component{
         const user = { name: "Tom",
                 gender: "Male",
                 picture: "",
-                description: "Hi I am use react",
-                major: "BEng",
-                university: "CU",
+                description: "Hi I am using react",
+                facalty: "Engineering",
+                university: "CUHK",
                 years: "3",
                 status: "A0",
-                interest: "nothing"};
+                interest: ["Dancing","Pop music","Classic music"] };
         var facalties=[ "Engineering",
                         "Medicine",
                         "Law",
@@ -48,76 +48,88 @@ class ProfileRegisterForm extends React.Component{
         return(
 
             <div className="ProfileRegisterForm_Container">
-                
                <div className="ProfileRegisterForm_row">
-                    <a className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Profile Picture: </a>
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Profile Picture: </label>
                     <a className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
                         <input type="file" id="ProfilePic" name="ProfilePic"></input>
                     </a>
                </div>
                <div className="ProfileRegisterForm_row">
-                    <a className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Name:</a>
-                    <a className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
-                        <input type="ProfileRegisterForm_input" id="ProfileName" name="ProfileName"></input>
-                    </a>
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Name:</label>
+                    <div className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
+                        <input className="ProfileRegisterForm_input" id="ProfileName" name="ProfileName" defaultValue={user.name}></input>
+                        <br></br>
+                    </div>
                </div>
                <div className="ProfileRegisterForm_row">
-                    <a className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Gender: </a>
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Status: </label>
+                    <div className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
+                        <input className="ProfileRegisterForm_input" id="Content" name="Status" defaultValue={user.status}></input>
+                    </div>
+               </div>
+               <div className="ProfileRegisterForm_row">
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Gender: </label>
                     <a className="ProfileRegisterForm_colm75" style={{padding:"0"}}type="ProfileRegisterForm_colm" >
                     {genders.map(function(gender){
+                        
                         return (
                         <a className="ProfileRegisterForm_colm30" type="ProfileRegisterForm_colm" id="Gender">
-                            <input type="radio" id={gender} name={gender}></input>
+                            <input type="radio" id={gender} name="gender" defaultChecked={gender == user.gender}></input>
                         <a>  {gender}</a>
                     </a>)
                     })}
                     </a>
                </div>
                <div className="ProfileRegisterForm_row">
-                    <a className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Description: </a>
-                    <a className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
-                        <textarea type="ProfileRegisterForm_text" style={{height:"100px"}}id="Description" name="Description"></textarea>
-                    </a>
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Description: </label>
+                    <div className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
+                        <textarea className="ProfileRegisterForm_input" style={{height:"100px"}}id="Description" name="Description" defaultValue={user.description}></textarea>
+                    </div>
                </div>
                <div className="ProfileRegisterForm_row">
-                    <a className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Facalty: </a>
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Facalty: </label>
                     <a className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
-                    <select type="ProfileRegisterForm_select" id="Facalty" name="Facalty">
+                    <select className="ProfileRegisterForm_input" id="Facalty" name="Facalty">
 
                         <option value="Nan"></option>
                         {facalties.map(function(facalty){
-                            return <option value={facalty}>{facalty}</option>
+                            return <option value={facalty} selected={facalty == user.facalty}>{facalty}</option>
                         })}
                     </select> 
                     </a>
                </div>
                <div className="ProfileRegisterForm_row">
-                    <a className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >University: </a>
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >University: </label>
                     <a className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
-                    <select type="ProfileRegisterForm_select" id="Facalty" name="Facalty">
+                    <select className="ProfileRegisterForm_input" id="Facalty" name="Facalty">
                     <option value="Nan"></option>
                     {universities.map(function(university){
-                            return <option  value={university}>{university}</option>
+                            return <option  value={university} selected={university == user.university}>{university}</option>
                     })}
                     </select>
                     </a>
 
                </div>
                <div className="ProfileRegisterForm_row">
-                    <a className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Status: </a>
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Status: </label>
                     <a className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
-                        <input type="ProfileRegisterForm_input" id="Status" name="Status"></input>
+                        <input className="ProfileRegisterForm_input" id="Status" name="Status" defaultValue={user.status}></input>
                     </a>
                </div>
 
                <div className="ProfileRegisterForm_row">
-                    <a className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Interests: </a>
+                    <label className="ProfileRegisterForm_colm25" type="ProfileRegisterForm_colm" >Interests: </label>
                     <div className="ProfileRegisterForm_colm75" type="ProfileRegisterForm_colm" >
                         <div className="ProfileRegisterForm_colm30">
                         {interests1.map(function(interest){
                             return (
                             <div style={{padding:"10px"}}type="ProfileRegisterForm_colm" id="Interest">
-                                <input type="checkbox" id={interest} name={interest} value={interest}></input>
+                                <input type="checkbox" 
+                                        id={interest} 
+                                        name={interest} 
+                                        value={interest} 
+                                        defaultChecked={user.interest.find(user_interest => user_interest == interest)}>
+                                </input>
                             <a>  {interest}</a>
                         </div>)
                         })}
