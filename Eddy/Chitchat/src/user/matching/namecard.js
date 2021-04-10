a599b8740a1a5b8d2819e0b7d227aaae192884e8
MYtom0825 <62584411+MYtom0825@users.noreply.github.com>
2021-04-10 22:30:39 +0800

    namecard and profile css change

--- Eddy/Chitchat/src/user/matching/namecard.js
+++ Eddy/Chitchat/src/user/matching/namecard.js
@@ -7,21 +7,21 @@ class Name_card extends React.Component {
   render() {
     return (
       <div>
-        <div className="card">
+        <div className="namecard_card">
           <img
-            className="card_img"
+            className="namecard_card_img"
             src="https://placeimg.com/400/400/tech"
             alt="John"
             width="270"
             height="270"
           ></img>
-          <h1 className="name">Tom Tom</h1>
-          <h3 className="info">Basic Info:</h3>
-          <p className="info">Hi</p>
-          <h3 className="info">Common Interest:</h3>
-          <p className="info">Hi</p>
-          <h3 className="info">Answer to the Pop-up Quiz:</h3>
-          <li className="info">Hi</li>
+          <h1 className="namecard_name">Tom Tom</h1>
+          <h3 className="namecard_info">Basic Info:</h3>
+          <p className="namecard_info">Hi</p>
+          <h3 className="namecard_info">Common Interest:</h3>
+          <p className="namecard_info">Hi</p>
+          <h3 className="namecard_info">Answer to the Pop-up Quiz:</h3>
+          <li className="namecard_info">Hi</li>
           <div></div>
           <p>
             <cardbutton>Contact</cardbutton>
