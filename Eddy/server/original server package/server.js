76bf962e86e5c96d9f5ae7deed75cb9fb8dbaced
thchang9 <changth85@gmail.com>
2021-04-11 22:49:51 +0800

    npm install mongoose auto-increment

--- Eddy/server/original server package/server.js
+++ Eddy/server/original server package/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('session');
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
 const socketio = require('socket.io');
 const http = require('http');
 
@@ -29,6 +30,7 @@ app.use(mission);
 //mongodb
 mongoose.connect("mongodb+srv://chit_chat:1230123@cluster0.4syir.mongodb.net/chit_chat?retryWrites=true&w=majority",{useNewUrlParser: true});
 var db = mongoose.connection;
+
 db.on('error',console.error.log(console,'MongoDB connection failed'));
 db.once('succeed',()=>{console.log('Successful connection')});
 
