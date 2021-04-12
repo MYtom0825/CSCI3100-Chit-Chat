96412ab9225afeb8fe4a2f721a4ee2e7d6b29205
mattyu122 <46863510+mattyu122@users.noreply.github.com>
2021-04-12 23:52:18 +0800

    .

--- Eddy/server/original/model/model_mission.js
+++ Eddy/server/original/model/model_mission.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 var MissionSchema = mongoose.Schema({
     _id: {type:mongoose.Schema.Types.ObjectId},
+    useraccount:{type:mongoose.Schema.Types.ObjectId,ref:'UserAccount'},
     missionID:{type:Number},
     Name:{type:String},
     Content:{type:String},
