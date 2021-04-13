af8172fee9f366da43c6389d3c057bb0418e34a3
thchang9 <63206814+thchang9@users.noreply.github.com>
2021-04-14 01:18:44 +0800

    update create

--- Eddy/server/router/router_create.js
+++ Eddy/server/router/router_create.js
@@ -12,78 +12,86 @@ router.use(cors());
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-router.post("/registration/:id", (req, res) => {
-  VerifyingAccount.findByIdAndRemove({ _id: req.params.id }, async function (err, record) {
-    if (err) {
-      console.log(err);
-    } else {
-      //insert into UserAccount
-      const user_id = new mongoose.Types.ObjectId();
-      const profile_id = new mongoose.Types.ObjectId();
-
-      var salt = "";
-      var hash = "";
-
-      console.log(record.email);
-      try {
-        salt = await bcrypt.genSaltSync(10);
-      } catch (err) {
-        console.log("sssss");
+router.post("/registration/:id", async (req, res) => {
+  const existdUserName = await UserAccount.findOne({ username: req.body.username });
+  if (existdUserName) {
+    return res.send("username taken");
+  }
+  else {
+    VerifyingAccount.findByIdAndRemove({ _id: req.params.id }, async function (err, record) {
+      if (err) {
         console.log(err);
+      } else {
+        //insert into UserAccount
+        const user_id = new mongoose.Types.ObjectId();
+        const profile_id = new mongoose.Types.ObjectId();
 
+        var salt = "";
+        var hash = "";
+
+        console.log(record.email);
+        console.log(record.password);
+        try {
+          salt = await bcrypt.genSaltSync(10);
+        } catch (err) {
+          console.log("sssss");
+          console.log(err);
+
+        }
+        try {
+          hash = await bcrypt.hashSync(record.password, salt);
+        } catch (err) {
+          console.log("xxxxxxx");
+          console.log(err);
+        }
+
+        var newUserAccount = new UserAccount({
+          _id: user_id,
+          email: record.email,
+          username: req.body.username,
+          password: hash,
+          onOffstatus: "off",
+          userProfile: profile_id,
+          token: 0,
+        });
+
+        var newUserProfile = new UserProfile({
+          _id: profile_id,
+          account: user_id,
+          picture: req.body.profilePic,
+          nickName: req.body.profileName,
+          //gender: req.body.gender,
+          university: req.body.university,
+          faculty: req.body.faculty,
+          major: req.body.major,
+          year: req.body.year,
+          status: req.body.status,
+          description: req.body.desc,
+          interest: req.body.interest, //array of interests
+          createdTime: Date.now(),
+          contact: req.body.contact, //ig
+        });
+
+        try {
+          await newUserAccount.save()
+          console.log("user account successfully created");
+        } catch (err) {
+          console.log("user account can't be created");
+          console.log(err);
+        }
+
+        try {
+          await newUserProfile.save()
+          console.log("user profile successfully created");
+        } catch (err) {
+          console.log("user profile can't be created");
+          console.log(err);
+        }
+
+        return res.send("Created");
       }
-      try {
-        hash = await bcrypt.hashSync(record.password, salt);
-      } catch (err) {
-        console.log("xxxxxxx");
-        console.log(err);
-      }
-
-      var newUserAccount = new UserAccount({
-        _id: user_id,
-        email: record.email,
-        username: req.body.username,
-        password: hash,
-        onOffstatus: "off",
-        userProfile: profile_id,
-        token: 0,
-      });
-
-      var newUserProfile = new UserProfile({
-        _id: profile_id,
-        account: user_id,
-        picture: req.body.profilePic,
-        nickName: req.body.profileName,
-        //gender: req.body.gender,
-        university: req.body.university,
-        faculty: req.body.faculty,
-        major: req.body.major,
-        year: req.body.year,
-        status: req.body.status,
-        interest: req.body.interest, //array of interests
-        createdTime: Date.now(),
-        contact: req.body.contact, //ig
-      });
-
-      try {
-        await newUserAccount.save()
-        console.log("user account successfully created");
-      } catch (err) {
-        console.log("user account can't be created");
-        console.log(err);
-      }
-
-      try {
-        await newUserProfile.save()
-        console.log("user profile successfully created");
-      } catch (err) {
-        console.log("user profile can't be created");
-        console.log(err);
-      }
-
-      return res.send("Created");
-    }
-  });
+    });
+  }
 
 });
 
