6508cdc1132a3fb40355cfccc27b095350cd29e8
Eddy-lcf <75269589+Eddy-lcf@users.noreply.github.com>
2021-04-15 02:08:08 +0800

    Update namecard.js

--- Eddy/Chitchat/src/user/matching/namecard.js
+++ Eddy/Chitchat/src/user/matching/namecard.js
@@ -12,11 +12,8 @@ class Name_card extends React.Component {
       <div>
         <div className='namecard_card'>
           <img className='namecard_card_img' src='https://placeimg.com/400/400/tech' alt='John' width='270' height='270'></img>
-          <h1 className='namecard_name'>Tom Tom</h1>
-          <h3 className='namecard_info'>Basic Info:</h3>
-          <p className='namecard_info'>Hi</p>
-          <h3 className='namecard_info'>Common Interest:</h3>
-          <p className='namecard_info'>Hi</p>
+          <h1 className='namecard_name'>ChitChat</h1>
+          <h3 className='namecard_info'>Welcome and enjoy your Chat</h3>
           <h3 className='namecard_info'>Answer to the Pop-up Quiz:</h3>
           {this.props.partnerresponse.userResponse ? this.props.partnerresponse.userResponse.map((answer) => <li className='namecard_info'>{answer}</li>) : ""}
           <div></div>
