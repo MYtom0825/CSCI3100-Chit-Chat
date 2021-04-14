4b3e7b4be48c41f4c2e3a6f563d3ee06ab8d5981
mattyu122 <46863510+mattyu122@users.noreply.github.com>
2021-04-14 15:59:31 +0800

    login request session username

--- Eddy/server/router/router_login.js
+++ Eddy/server/router/router_login.js
@@ -11,6 +11,75 @@ router.use(cors());
 
 router.post("/login", (req, res) => {
   //login
+
+
+
+  if(req.session.username){
+    UserAccount.findOne({username:req.session.username}).populate("userProfile").exec(function(error,user){
+      if (error) {
+        console.log(err);
+        res.send("fail");
+        return;
+      }else if (!user) {
+        var data = {
+          loginstate: 0,
+        };
+        console.log("can't find user req session ");
+        return res.json(data);
+      }else{
+        user.onOffStatus = "on";
+
+        var data = {
+          loginstate: 2,
+          name: user.userProfile.nickName,
+          gender: user.userProfile.gender,
+          picture: user.userProfile.picture,
+          description: user.userProfile.description,
+          faculty: user.userProfile.faculty,
+          university: user.userProfile.university,
+          year: user.userProfile.year,
+          status: user.userProfile.status,
+          interest: user.userProfile.interest,
+          ig: user.userProfile.contact,
+          token: user.token,
+        };
+
+        Mission.exists({ useraccount: user._id, missionID: 0 }, function (err, exist) {
+          if (err) {
+            console.log("mission exist error");
+          } else if (exist == false) {
+            var missionFinished = new Mission({
+              _id: new mongoose.Types.ObjectId(),
+              useraccount: user._id,
+              missionID: 0,
+              Name: "Daily Login",
+              Content: "Log in daily",
+              token: 5,
+            });
+
+            missionFinished.save((error) => {
+              if (error) {
+                console.log(error);
+              }
+            });
+            user.token += 5;
+            user.save((err) => {
+              if (err) {
+                console.log("token can't be added to the account");
+              } else {
+                console.log("token added to the account");
+              }
+            });
+          } else if (exist == true) {
+            console.log("mission completed before");
+          }
+        });
+
+        console.log("login successful");
+        return res.json(data);
+      }
+    });
+  }else{
   console.log("log in");
   var email = req.body["email"];
   var password = req.body["password"];
@@ -96,23 +165,8 @@ router.post("/login", (req, res) => {
         return res.json(data);
       }
     });
-
-  /*if (username && email && password) {    // to be modified
-        //connect to mongodb to check...
-        if (true) { //login success
-            request.session.loggedin = true;
-      request.session.username = username;
-      response.redirect('/');
-        }
-        else {  //login failed
-            response.send('Incorrect Username and/or Password!');
-            res.end();
-        }
-    }
-    else {
-        res.send('Please enter Username, Email and Password!');
-        res.end();
-    }*/
+  }
+  
 });
 
 router.post("/forgotpw", (req, res) => {
