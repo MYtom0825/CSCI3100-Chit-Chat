020e8dcf058afbd0301dbe0a6981b868223a1830
mattyu122 <46863510+mattyu122@users.noreply.github.com>
2021-04-14 20:11:02 +0800

    Update router_mission.js

--- Eddy/server/router/router_mission.js
+++ Eddy/server/router/router_mission.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const session = require("express-session");
 const router = express.Router();
 let UserAccount = require("../model/model_account.js");
 let Mission=require('../model/model_mission.js');
@@ -16,33 +15,27 @@ router.get("/mission", (req, res) => {
     return res.status(401).send();
   }
 
-  
-
   UserAccount.findOne({ username: req.body.username }, function (err, result) {
-    var missionFinished=[];
-     
-    function missionFind(useraccount){
-
-      Mission.find({useraccount:useraccount}).sort({missionID:'ascending'}).exec(function(err,missionArray){
+    
+    if (err) {
+      res.send(err);
+    } else if(result==null){
+      res.send("user cant be found");
+    }
+    else {
+      Mission.find({useraccount:result.id}).sort({missionID:'ascending'}).exec(function(err,missionArray){
+        var missionFinished=[];
         if(err){
           console.log("mission find error");
           console.log(err);
         }else{
+           console.log(missionArray);
            missionArray.forEach(element=>missionFinished.push(element.missionID));
-           return;
+           return res.json({
+            missionFinishedID: missionFinished,
+          });
         }
         });
-
-    }
-
-
-    if (err) {
-      res.send(err);
-    } else {
-        missionFind(result._id);
-      return res.json({
-        missionFinishedID: missionFinished,
-      });
     }
   });
 });
